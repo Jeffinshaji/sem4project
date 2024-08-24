1f5da8cad1e79f65473cfe0f9384432c15fc8b60
Adharsh jaison <adharsh.jaison@msds.christuniversity.in>
2024-08-24 07:56:45 +0530

    updated

--- RoadSafety/Accident/urls.py
+++ RoadSafety/Accident/urls.py
@@ -2,7 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name = 'home')
+    path('',views.index,name='home'),
+    path('about',views.about,name='about'),
+    path('service',views.service,name='service'),
+    path('team',views.team,name='team')
     
-   
    ]
--- RoadSafety/Accident/views.py
+++ RoadSafety/Accident/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from . import views
 # Create your views here.
 def index(request):
     return render (request,'index.html')
+def about(request):
+    return render (request,'about.html')
+def service(request):
+    return render (request,'service.html')
+
+def team(request):
+    return render (request,'team.html')
