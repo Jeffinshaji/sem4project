da6dca83415671791dbd1cf3b9bf63b1f31d19d8
Jeffin Shaji <jeffinshaji1997@gmail.com>
2024-09-09 13:32:32 +0530

    update

--- RoadSafety/RoadSafety/urls.py
+++ RoadSafety/RoadSafety/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from schema_graph.views import Schema
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Accident.urls'))
+    path('', include('Accident.urls')),
+    path('admin_tools_stats/', include('admin_tools_stats.urls')),
+    path("schema/", Schema.as_view()),
    
    ]
